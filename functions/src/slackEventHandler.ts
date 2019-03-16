38b96c9083874df5b3968b1fbb2939a7dd3221b0
sesta <sesta.ec5@gmail.com>
2019-03-16 22:31:48 +0900

    メッセージの内容を受け取れるようにする

--- functions/src/slackEventHandler.ts
+++ functions/src/slackEventHandler.ts
@@ -1,9 +1,18 @@
 export function getSlackEventResponse(body: any): any|undefined {
   if (body.event.bot_id !== undefined) {
-    console.log(`botからのリクエストです: ${body.event.bot_id}`)
+    console.log(`botの発言です。 bot_id: ${body.event.bot_id}`)
 
     return
   }
+
+  if (body.event.type !== 'message') {
+    console.log(`予期せぬイベントタイプです。type: ${body.event.type}`)
+
+    return
+  }
+
+  const message: string = body.event.text
+  console.log(message)
 
   return
 }
